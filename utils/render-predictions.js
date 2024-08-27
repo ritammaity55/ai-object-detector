a630ce74c473d66a610d542834ae834ae8624257
Ritam Maity <ritammaity55@gmail.com>
2024-08-28 03:47:09 +0530

    Update render-predictions.js

--- utils/render-predictions.js
+++ utils/render-predictions.js
@@ -3,7 +3,6 @@ import {throttle} from "lodash";
 export const renderPredictions = (predictions, ctx) => {
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-  // Fonts
   const font = "16px sans-serif";
   ctx.font = font;
   ctx.textBaseline = "top";
@@ -13,16 +12,13 @@ export const renderPredictions = (predictions, ctx) => {
 
     const isPerson = prediction.class === "person";
 
-    // bounding box
     ctx.strokeStyle = isPerson ? "#FF0000" : "#00FFFF";
     ctx.lineWidth = 4;
     ctx.strokeRect(x, y, width, height);
 
-    // fill the color
-    ctx.fillStyle = `rgba(255, 0, 0, ${isPerson ? 0.2 : 0})`; // Set the fill color to red
+    ctx.fillStyle = `rgba(255, 0, 0, ${isPerson ? 0.2 : 0})`; 
     ctx.fillRect(x, y, width, height);
 
-    // Draw the label background.
     ctx.fillStyle = isPerson ? "#FF0000" : "#00FFFF";
     const textWidth = ctx.measureText(prediction.class).width;
     const textHeight = parseInt(font, 10); // base 10
